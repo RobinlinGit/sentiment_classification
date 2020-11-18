06d18612933311d6ad718a3cc9a1f96eaf34661c
Zhonghang <robinlin163@163.com>
2020-11-18 14:59:37 +0800

    stage change

--- fasttext_train.py
+++ fasttext_train.py
@@ -16,12 +16,13 @@ lr = 1
 minCount = 5
 folder = "fasttext-data-char"
 columns = [
-    'location_traffic_convenience',
-    'location_distance_from_business_district', 'location_easy_to_find',
-    'service_wait_time', 'service_waiters_attitude',
-    'service_parking_convenience', 'service_serving_speed', 'price_level',
-    'price_cost_effective', 'price_discount', 'environment_decoration',
-    'environment_noise', 'environment_space', 'environment_clean',
+    # 'location_traffic_convenience',
+    # 'location_distance_from_business_district', 'location_easy_to_find',
+    # 'service_wait_time', 'service_waiters_attitude',
+    # 'service_parking_convenience', 'service_serving_speed', 'price_level',
+    # 'price_cost_effective', 'price_discount', 'environment_decoration',
+    # 'environment_noise', 'environment_space', 
+     'environment_clean',
     'dish_portion', 'dish_taste', 'dish_look', 'dish_recommendation',
     'others_overall_experience', 'others_willing_to_consume_again'
 ]
